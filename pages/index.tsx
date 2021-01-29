6249060568c5eb060c0cfe96922ab8ebb6aa3629
disj11 <disj11@naver.com>
2021-01-29 15:10:32 +0900

    테마 토글 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,19 @@ import {inject, observer} from "mobx-react";
 import {GetServerSideProps, NextPage} from "next";
 import {UserRepository} from "../src/repositories/user_repository";
 import {UserStore} from "../src/stores/user_store";
+import {
+    Box,
+    Button,
+    Container,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@material-ui/core";
+import {ToggleThemeContext} from "../src/theme";
 
 interface Props {
     userStore: UserStore,
@@ -12,21 +25,40 @@ interface Props {
 const Home: NextPage<Props> = inject("userStore")(
     observer((props) => {
         const {userStore} = props;
+        const {toggleTheme} = React.useContext(ToggleThemeContext);
+
         return (
             <>
                 <Head>
                     <title>Next.js + React + Mobx</title>
                 </Head>
-                <div>
-                    {userStore.getUsers().map(user => (
-                        <div key={user.id}>
-                            <div>{user.id}</div>
-                            <div>{user.email}</div>
-                            <div>{user.name}</div>
-                            <div>{user.username}</div>
-                        </div>
-                    ))}
-                </div>
+                <Container maxWidth="lg">
+                    <Box mb={3}>
+                        <Button onClick={toggleTheme}>Toggle theme</Button>
+                    </Box>
+                    <TableContainer component={Paper}>
+                        <Table aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Id</TableCell>
+                                    <TableCell align="right">Username</TableCell>
+                                    <TableCell align="right">Name</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {userStore.getUsers().map((user) => (
+                                    <TableRow key={user.id}>
+                                        <TableCell component="th" scope="row">
+                                            {user.name}
+                                        </TableCell>
+                                        <TableCell align="right">{user.username}</TableCell>
+                                        <TableCell align="right">{user.email}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Container>
             </>
         );
     })
--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,52 +1,52 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { MuiThemeProvider, CssBaseline } from '@material-ui/core'
-import { lightTheme, darkTheme } from './theme'
-import { Theme } from '@material-ui/core'
+import React, {useCallback, useEffect, useState} from 'react'
+import {MuiThemeProvider, CssBaseline} from '@material-ui/core'
+import {lightTheme, darkTheme} from './theme'
+import {Theme} from '@material-ui/core'
 
 interface ThemeProviderProps {
-  children: React.ReactNode
+    children: React.ReactNode
 }
 
 export const ToggleThemeContext = React.createContext({
-  toggleTheme: () => {
-    console.log()
-  },
-  isDark: false,
-})
+    toggleTheme: () => {
+        console.log();
+    },
+    isDark: false,
+});
 
 type SelectedTheme = {
-  themeName: string
-  appliedTheme: Theme
+    themeName: string
+    appliedTheme: Theme
 }
 
-export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }: ThemeProviderProps) => {
-  const [selectedTheme, setSelectedTheme] = useState<SelectedTheme>({ appliedTheme: darkTheme, themeName: 'darkTheme' })
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({children}: ThemeProviderProps) => {
+    const [selectedTheme, setSelectedTheme] = useState<SelectedTheme>({appliedTheme: darkTheme, themeName: 'darkTheme'});
 
-  useEffect(() => {
-    const theme = localStorage.getItem('theme')
-    if (theme && theme === 'lightTheme') {
-      setSelectedTheme({ appliedTheme: lightTheme, themeName: 'lightTheme' })
-    }
-  }, [])
+    useEffect(() => {
+        const theme = localStorage.getItem('theme');
+        if (theme && theme === 'lightTheme') {
+            setSelectedTheme({appliedTheme: lightTheme, themeName: 'lightTheme'});
+        }
+    }, []);
 
-  const toggleTheme = useCallback(() => {
-    if (!selectedTheme || selectedTheme.themeName === 'darkTheme') {
-      setSelectedTheme({ appliedTheme: lightTheme, themeName: 'lightTheme' })
-      localStorage.setItem('theme', 'lightTheme')
-    } else {
-      setSelectedTheme({ appliedTheme: darkTheme, themeName: 'darkTheme' })
-      localStorage.setItem('theme', 'darkTheme')
-    }
-  }, [selectedTheme, setSelectedTheme])
+    const toggleTheme = useCallback(() => {
+        if (!selectedTheme || selectedTheme.themeName === 'darkTheme') {
+            setSelectedTheme({appliedTheme: lightTheme, themeName: 'lightTheme'});
+            localStorage.setItem('theme', 'lightTheme');
+        } else {
+            setSelectedTheme({appliedTheme: darkTheme, themeName: 'darkTheme'});
+            localStorage.setItem('theme', 'darkTheme');
+        }
+    }, [selectedTheme, setSelectedTheme]);
 
-  return (
-    <ToggleThemeContext.Provider value={{ toggleTheme, isDark: !(selectedTheme.themeName === 'lightTheme') }}>
-      <MuiThemeProvider theme={selectedTheme.appliedTheme}>
-        <CssBaseline />
-        {children}
-      </MuiThemeProvider>
-    </ToggleThemeContext.Provider>
-  )
-}
+    return (
+        <ToggleThemeContext.Provider value={{toggleTheme, isDark: !(selectedTheme.themeName === 'lightTheme')}}>
+            <MuiThemeProvider theme={selectedTheme.appliedTheme}>
+                <CssBaseline/>
+                {children}
+            </MuiThemeProvider>
+        </ToggleThemeContext.Provider>
+    )
+};
 
 export default ThemeProvider
